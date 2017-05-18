5bb1b40bed0c30288bcb6182b1783c8d318fcb30
Florian Warzecha <flowa2000@gmail.com>
2017-05-18 18:24:34 +0200

    reworked SendMessages thread
    - support for sending documents by their fileId
    - add methods simplified by using java.util.Optional class
       -> markdown and inlineKeyboardMarkup parameters can be an empty optional (Optional.empty()) if not needed
       -> if needed one can specify Optional.of(value)
       -> reduced number of almost redundant methods where only one parameter was different
    - all queues are final
    - deletion of old values in the hashmap is simpler

--- bot/src/main/java/org/telegram/bot/api/SendMessages.java
+++ bot/src/main/java/org/telegram/bot/api/SendMessages.java
@@ -1,16 +1,21 @@
 package org.telegram.bot.api;
 
 import org.telegram.bot.database.DatabaseException;
+import org.telegram.bot.messages.InlineKeyboard;
+import org.telegram.telegrambots.api.methods.send.SendDocument;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.bots.AbsSender;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.logging.BotLogger;
 
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * @author liketechnik
@@ -24,7 +29,7 @@ public class SendMessages extends Thread {
     private static volatile SendMessages instance;
 
     public enum types {
-        send_message, edit_message
+        send_message, edit_message, document_message
     }
 
     private SendMessages() {
@@ -37,6 +42,7 @@ public class SendMessages extends Thread {
         inlineKeyboards = new ConcurrentHashMap<>();
         messageTypes = new ConcurrentHashMap<>();
         messageIds = new ConcurrentHashMap<>();
+        fileIds = new ConcurrentHashMap<>();
     }
 
     public static SendMessages getInstance() {
@@ -54,18 +60,23 @@ public class SendMessages extends Thread {
         return currentInstance;
     }
 
-    private LinkedBlockingQueue<Integer> messageHashes;
-    private ConcurrentHashMap<Integer, String> messageTypes;
+    private final LinkedBlockingQueue<Integer> messageHashes;
+    private final ConcurrentHashMap<Integer, String> messageTypes;
 
 
-    private ConcurrentHashMap<Integer, AbsSender> absSender;
-    private ConcurrentHashMap<Integer, String> chatIds;
+    private final ConcurrentHashMap<Integer, AbsSender> absSender;
+    private final ConcurrentHashMap<Integer, String> chatIds;
 
-    private ConcurrentHashMap<Integer, String> messages;
-    private ConcurrentHashMap<Integer, Boolean> enableMarkdown;
-    private ConcurrentHashMap<Integer, InlineKeyboardMarkup> inlineKeyboards;
+    private final ConcurrentHashMap<Integer, String> messages;
+    private final ConcurrentHashMap<Integer, Boolean> enableMarkdown;
 
-    private ConcurrentHashMap<Integer, Integer> messageIds;
+    private final ConcurrentHashMap<Integer, InlineKeyboardMarkup> inlineKeyboards;
+
+    private final ConcurrentHashMap<Integer, Integer> messageIds;
+
+    private final ConcurrentHashMap<Integer, String> fileIds;
+
+    private final boolean enableMarkdownDefault = false;
 
     @Override
     public void run() {
@@ -98,11 +109,36 @@ public class SendMessages extends Thread {
                     String currentChatId = this.chatIds.remove(currentMessageHash);
                     AbsSender currentAbsSender = this.absSender.remove(currentMessageHash);
                     Integer currentMessageId = this.messageIds.remove(currentMessageHash);
+                    boolean currentEnableMarkdown = this.enableMarkdown.remove(currentMessageHash);
 
+                    try {
+                        message.setReplyMarkup(this.inlineKeyboards.remove(currentMessageHash));
+                    } catch (NullPointerException ignored) {
+
+                    }
+
+                    message.enableMarkdown(currentEnableMarkdown);
                     message.setText(currentMessage);
                     message.setMessageId(currentMessageId);
                     message.setChatId(currentChatId);
                     currentAbsSender.editMessageText(message);
+                } else if (currentType.equals(types.document_message.name())) {
+                    SendDocument message = new SendDocument();
+                    String currentMessage = this.messages.remove(currentMessageHash);
+                    String currentChatId = this.chatIds.remove(currentMessageHash);
+                    AbsSender currentAbsSender = this.absSender.remove(currentMessageHash);
+                    String currentFileId = this.fileIds.remove(currentMessageHash);
+
+                    try {
+                        message.setReplyMarkup(this.inlineKeyboards.remove(currentMessageHash));
+                    } catch (NullPointerException ignored) {
+
+                    }
+
+                    message.setCaption(currentMessage);
+                    message.setChatId(currentChatId);
+                    message.setDocument(currentFileId);
+                    currentAbsSender.sendDocument(message);
                 } else {
                     BotLogger.error(LOGTAG, "Registered message without known type.");
                 }
@@ -110,73 +146,52 @@ public class SendMessages extends Thread {
             } catch (InterruptedException | TelegramApiException e) {
                 BotLogger.error(LOGTAG, e);
             } finally {
-                try {
-                    this.messages.remove(removalMessageHash);
-                } catch (NullPointerException ignored) {
-
-                } try {
-                    this.absSender.remove(removalMessageHash);
-                } catch (NullPointerException ignored) {
-
-                } try {
-                    this.chatIds.remove(removalMessageHash);
-                } catch (NullPointerException ignored) {
-
-                } try {
-                    this.enableMarkdown.remove(removalMessageHash);
-                } catch (NullPointerException ignored) {
-
-                } try {
-                    this.inlineKeyboards.remove(removalMessageHash);
-                } catch (NullPointerException ignored) {
-
-                } try {
-                    this.messageTypes.remove(removalMessageHash);
-                } catch (NullPointerException ignored) {
-
-                } try {
-                    this.messageIds.remove(removalMessageHash);
-                } catch (NullPointerException ignored) {
-
-                }
+                this.messages.remove(removalMessageHash);
+                this.absSender.remove(removalMessageHash);
+                this.chatIds.remove(removalMessageHash);
+                this.enableMarkdown.remove(removalMessageHash);
+                this.inlineKeyboards.remove(removalMessageHash);
+                this.messageTypes.remove(removalMessageHash);
+                this.messageIds.remove(removalMessageHash);
+                this.fileIds.remove(removalMessageHash);
             }
         }
     }
 
-    public void addMessage(Integer messageHash, String message, String chatId, AbsSender absSender) {
+    public void addMessage(Integer messageHash, String message, String chatId, AbsSender absSender, Optional<Boolean> enableMarkdown, Optional<InlineKeyboardMarkup> inlineKeyboard) {
         this.messages.putIfAbsent(messageHash, message);
         this.chatIds.putIfAbsent(messageHash, chatId);
         this.absSender.putIfAbsent(messageHash, absSender);
-        this.enableMarkdown.putIfAbsent(messageHash, false);
+        this.enableMarkdown.putIfAbsent(messageHash, enableMarkdown.orElse(this.enableMarkdownDefault));
+        inlineKeyboard.ifPresent(inlineKeyboardMarkup -> {
+            this.inlineKeyboards.putIfAbsent(messageHash, inlineKeyboardMarkup);
+        });
         this.messageTypes.putIfAbsent(messageHash, types.send_message.name());
         this.messageHashes.add(messageHash);
     }
 
-    public void addMessage(Integer messageHash, String message, String chatId, AbsSender absSender, boolean enableMarkdown) {
+    public void addEditMessage(Integer messageHash, String message, String chatId, AbsSender absSender, Integer messageID, Optional<Boolean> enableMarkdown, Optional<InlineKeyboardMarkup> inlineKeyboard) {
         this.messages.putIfAbsent(messageHash, message);
         this.chatIds.putIfAbsent(messageHash, chatId);
         this.absSender.putIfAbsent(messageHash, absSender);
-        this.enableMarkdown.putIfAbsent(messageHash, enableMarkdown);
-        this.messageTypes.putIfAbsent(messageHash, types.send_message.name());
-        this.messageHashes.add(messageHash);
-    }
-
-    public void addMessage(Integer messageHash, String message, String chatId, AbsSender absSender, InlineKeyboardMarkup inlineKeyboard) {
-        this.messages.putIfAbsent(messageHash, message);
-        this.chatIds.putIfAbsent(messageHash, chatId);
-        this.absSender.putIfAbsent(messageHash, absSender);
-        this.enableMarkdown.putIfAbsent(messageHash, false);
-        this.inlineKeyboards.putIfAbsent(messageHash, inlineKeyboard);
-        this.messageTypes.putIfAbsent(messageHash, types.send_message.name());
-        this.messageHashes.add(messageHash);
-    }
-
-    public void addEditMessage(Integer messageHash, String message, String chatId, AbsSender absSender, Integer messageID) {
-        this.messages.putIfAbsent(messageHash, message);
-        this.chatIds.putIfAbsent(messageHash, chatId);
-        this.absSender.putIfAbsent(messageHash, absSender);
+        this.enableMarkdown.putIfAbsent(messageHash, enableMarkdown.orElse(this.enableMarkdownDefault));
         this.messageIds.putIfAbsent(messageHash, messageID);
+        inlineKeyboard.ifPresent(inlineKeyboardMarkup -> {
+            this.inlineKeyboards.putIfAbsent(messageHash, inlineKeyboardMarkup);
+        });
         this.messageTypes.putIfAbsent(messageHash, types.edit_message.name());
+        this.messageHashes.add(messageHash);
+    }
+
+    public void addDocumentMessage(Integer messageHash, String caption, String chatId, AbsSender absSender, String fileId, Optional<InlineKeyboardMarkup> inlineKeyboard) {
+        this.messages.putIfAbsent(messageHash, caption);
+        this.chatIds.putIfAbsent(messageHash, chatId);
+        this.absSender.putIfAbsent(messageHash, absSender);
+        this.fileIds.putIfAbsent(messageHash, fileId);
+        inlineKeyboard.ifPresent(inlineKeyboardMarkup -> {
+            this.inlineKeyboards.putIfAbsent(messageHash, inlineKeyboardMarkup);
+        });
+        this.messageTypes.putIfAbsent(messageHash, types.document_message.name());
         this.messageHashes.add(messageHash);
     }
 
